f0d8d9a9f35b9c8c2448688a294dce3abdac5a13
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-07-04 18:46:47 +0530

    style added woman svg to login page && feat addedlogin error message to password input
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,45 +1,49 @@
-'use client'
-
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import Link from 'next/link'
-
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import loginSvg from "../../public/login.svg";
+import Image from "next/image";
+import Link from "next/link";
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [view, setView] = useState('sign-in')
-  const router = useRouter()
-  const supabase = createClientComponentClient()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [view, setView] = useState("sign-in");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
   const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     await supabase.auth.signUp({
       email,
       password,
       options: {
         emailRedirectTo: `${location.origin}/auth/callback`,
       },
-    })
-    setView('check-email')
-  }
+    });
+    setView("check-email");
+  };
 
   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    await supabase.auth.signInWithPassword({
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
-    })
-    router.push('/')
-    router.refresh()
-  }
+    });
+    if (error) {
+      setError(error.message);
+      return;
+    }
+    router.push("/");
+    router.refresh();
+  };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="grid flex-1 w-full h-screen grid-cols-2 gap-2 px-8 ">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
+        className="absolute flex items-center px-4 py-2 text-sm no-underline rounded-md left-8 top-8 text-foreground bg-btn-background hover:bg-btn-background-hover group">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -50,70 +54,73 @@ export default function Login() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
+          className="w-4 h-4 mr-2 transition-transform group-hover:-translate-x-1">
           <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
+        </svg>{" "}
         Back
       </Link>
-      {view === 'check-email' ? (
+      {view === "check-email" ? (
         <p className="text-center text-foreground">
           Check <span className="font-bold">{email}</span> to continue signing
           up
         </p>
       ) : (
         <form
-          className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-          onSubmit={view === 'sign-in' ? handleSignIn : handleSignUp}
-        >
+          className="flex flex-col justify-center flex-1 w-full gap-2 text-foreground"
+          onSubmit={view === "sign-in" ? handleSignIn : handleSignUp}>
           <label className="text-md" htmlFor="email">
             Email
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="px-4 py-2 mb-6 border rounded-md bg-inherit"
             name="email"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             placeholder="you@example.com"
           />
-          <label className="text-md" htmlFor="password">
-            Password
-          </label>
+          <div className="flex flex-row justify-between">
+            <label className="text-md" htmlFor="password">
+              Password
+            </label>
+            <span className="text-red-500">{error}</span>
+          </div>
+
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className={`px-4 py-2 mb-6 ${
+              error && " border-red-500"
+            } border rounded-md bg-inherit`}
             type="password"
             name="password"
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             placeholder="••••••••"
           />
-          {view === 'sign-in' && (
+
+          {view === "sign-in" && (
             <>
-              <button className="bg-green-700 rounded px-4 py-2 text-white mb-6">
+              <button className="px-4 py-2 mb-6 text-white bg-green-700 rounded">
                 Sign In
               </button>
               <p className="text-sm text-center">
                 Don't have an account?
                 <button
                   className="ml-1 underline"
-                  onClick={() => setView('sign-up')}
-                >
+                  onClick={() => setView("sign-up")}>
                   Sign Up Now
                 </button>
               </p>
             </>
           )}
-          {view === 'sign-up' && (
+          {view === "sign-up" && (
             <>
-              <button className="bg-green-700 rounded px-4 py-2 text-white mb-6">
+              <button className="px-4 py-2 mb-6 text-white bg-green-700 rounded">
                 Sign Up
               </button>
               <p className="text-sm text-center">
                 Already have an account?
                 <button
                   className="ml-1 underline"
-                  onClick={() => setView('sign-in')}
-                >
+                  onClick={() => setView("sign-in")}>
                   Sign In Now
                 </button>
               </p>
@@ -121,6 +128,9 @@ export default function Login() {
           )}
         </form>
       )}
+      <div className="flex flex-col justify-center flex-1 w-full gap-2 text-foreground">
+        <Image src={loginSvg} alt="Login" />
+      </div>
     </div>
-  )
+  );
 }
